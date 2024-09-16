b0fd7f611be4e21b11668cda94cf5511bf75d892
MaxD-sheridan <dabski@sheridancollege.ca>
2024-09-16 16:51:11 -0400

    added welcome message

--- Documents/NetBeansProjects/Demo01/src/demo01/Demo01.java
+++ Documents/NetBeansProjects/Demo01/src/demo01/Demo01.java
@@ -15,6 +15,7 @@ public class Demo01 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Welcome to Git Version Control!");
     }
     
 }
